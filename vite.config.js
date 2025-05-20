1e922c8cab18af33b420fb8826721cc49928d130
Uroosa Imtiaz <50160261+uroosaimtiaz@users.noreply.github.com>
2025-05-19 22:16:04 -0400

    chore: deploy Vite+Svelte blog to user site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',                 // root path
   plugins: [svelte()],
-})
+  build: {
+    outDir: 'docs'           // temporary build dir
+  }
+});
